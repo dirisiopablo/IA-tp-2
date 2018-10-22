26da4cd9189079d3ef3781cd4ba81ae649562136
pdirisio <pdirisio>
2018-10-21 21:51:51 -0300

    el predict usa el train tensor

--- predict.py
+++ predict.py
@@ -13,9 +13,9 @@ with tf.Session() as sess:
 	input_layer = graph.get_tensor_by_name('input_layer:0')
 	output_layer = graph.get_tensor_by_name('output_layer:0')
 	# pick a user
-	X_test = graph.get_tensor_by_name('X_test:0')
+	X_test = graph.get_tensor_by_name('X_train:0')
 	X_test_np = X_test.eval()
-	sample_user = X_test_np[98,:]
+	sample_user = X_test_np[99,:]
 	#get the predicted ratings
 	print('\nGet predicted ratings for user:')
 	user_pred = sess.run(output_layer, feed_dict={input_layer:[sample_user]})
